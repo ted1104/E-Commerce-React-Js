93e055dfc0b7e48a98f811a182141a1a853a14e8
Teddy Walter <teddywalter2016@gmail.com>
2021-01-28 15:48:28 +0000

    Home Page

---
+++ src/HomePage.component.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const HomePage = () =>(
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>HATS</h1>
+                    <h2 className='subtitle'>SHOP NOW</h2>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>WOMEN</h1>
+                    <h2 className='subtitle'>SHOP NOW</h2>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>JACKET</h1>
+                    <h2 className='subtitle'>SHOP NOW</h2>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>HATS</h1>
+                    <h2 className='subtitle'>SHOP NOW</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage;
