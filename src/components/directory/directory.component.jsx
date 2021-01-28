17e0d17ed8df8c0a828e06774f856c4226015726
Teddy Walter <teddywalter2016@gmail.com>
2021-01-28 22:50:21 +0000

    Bloc produit dynamic

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -47,8 +47,8 @@ export class Directory extends React.Component{
     render(){
         return (
             <div className='directory-menu'>
-                { this.state.sections.map( ({ title, imageUrl, id}) => (
-                    <MenuItem title = {title} />
+                { this.state.sections.map( ({ id,title, imageUrl, size}) => (
+                    <MenuItem key={id} title = {title} imageUrl = {imageUrl} size={size}/>
                 ))}
             </div>
         )
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import './menu-item.styles.scss'
 
-const MenuItem = ({title}) =>(
-    <div className="menu-item">
+const MenuItem = ({ title, imageUrl,size }) =>(
+    <div 
+    className={`${size} menu-item`} 
+    style={{
+        backgroundImage : `url(${ imageUrl })`
+    }}>
         <div className='content'>
             <h1 className='title'>{title}</h1>
             <h2 className='subtitle'>SHOP NOW</h2>
