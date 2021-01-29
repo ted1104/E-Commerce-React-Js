58b48ad667505d2284c1ee50154e722b79412f23
Teddy Walter <teddywalter2016@gmail.com>
2021-01-29 14:37:06 +0000

    Basic router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
 import './App.css';
-import HomePage from './pages/homepage/HomePage.component'
+// import HomePage from './pages/homepage/HomePage.component'
+import {Route, Link} from 'react-router-dom';
+
+const HomePage = (props) =>{
+  console.log(props);
+  return(
+    <div>
+      <h1>HOME PAGE</h1> 
+      {/* DEUX MANIERES DE NAVIGUER SUR REACT */}
+      <Link to="/topics">Topics</Link>
+      <button onClick={()=>props.history.push('/topics')}>Topics</button>
+    </div>
+  );
+};
+
+const TopicList = (props) =>{
+  console.log(props);
+  return(
+    <div>
+      <h1>TOPIC LIST PAGE</h1>
+      <Link to={`${props.match.url}/12`}>Detail #12</Link>
+      <Link to={`${props.match.url}/13`}>Detail #13</Link>
+      <Link to={`${props.match.url}/14`}>Detail #14</Link>
+    </div>
+  );
+};
+
+const TopicDetail = (props) =>{
+  console.log(props);
+  return(
+    <div>
+      <h1>TOPIC DETAIL PAGE {props.match.params.topicId}</h1>
+    </div>
+  );
+};
 
 function App() {
+  
   return (
     <div className="App">
-      <HomePage />
+     
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/blog/content/topics' component = {TopicList} />
+      <Route exact path = '/blog/content/topics/:topicId' component = {TopicDetail} />
+  
     </div>
   );
 }
