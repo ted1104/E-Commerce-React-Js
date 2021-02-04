44edb583298b1f8e7fcbb6b85b84181f1b2f4d17
Teddy Walter <teddywalter2016@gmail.com>
2021-02-04 15:12:06 +0000

    get data for user connected with google account

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import './App.css';
-import HomePage from './pages/homepage/HomePage.component'
-import {Route, Switch} from 'react-router-dom';
-import ShopPage from './pages/shop/shoppage.component'
-import Header from './components/header/header.component'
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+import React from "react";
+import "./App.css";
+import HomePage from "./pages/homepage/HomePage.component";
+import { Route, Switch } from "react-router-dom";
+import ShopPage from "./pages/shop/shoppage.component";
+import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import { auth } from "./firebase/firebase.utils";
+
 // const HomePage = (props) =>{
 //   console.log(props);
 //   return(
 //     <div>
-//       <h1>HOME PAGE</h1> 
+//       <h1>HOME PAGE</h1>
 //       {/* DEUX MANIERES DE NAVIGUER SUR REACT */}
 //       <Link to="/topics">Topics</Link>
 //       <button onClick={()=>props.history.push('/topics')}>Topics</button>
@@ -44,21 +47,40 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 //     </div>
 //   )
 // }
-function App() {
-  
-  return (
-    <div className="App">
-      <Header/>
-     <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop' component = {ShopPage} />
-        <Route exact path ='/signin' component = {SignInAndSignUpPage}/>
-        {/* <Route exact path='/blog/content/topics' component = {TopicList} />
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null,
+    };
+  }
+  unsucribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsucribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsucribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/signin" component={SignInAndSignUpPage} />
+          {/* <Route exact path='/blog/content/topics' component = {TopicList} />
         <Route exact path = '/blog/content/topics/:topicId' component = {TopicDetail} /> */}
-     </Switch>
-      
-    </div>
-  );
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
