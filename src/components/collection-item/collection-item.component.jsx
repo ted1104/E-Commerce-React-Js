e7811fcb6e672edf0b35b49ecbb82d28b6bc3707
Teddy Walter <teddywalter2016@gmail.com>
2021-02-10 22:55:21 +0000

    implements CartItem reduce

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,16 +1,27 @@
 import React from "react";
+import { connect } from "react-redux";
 import "./collection-item.styles.scss";
 import CustomButtom from "../custom-buttom/custom-buttom.component";
-
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className="collection-item">
-    <div style={{ backgroundImage: `url(${imageUrl}` }} className="image"></div>
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
+import { addItem } from "../../redux/cart/cart.actions";
+const CollectionItem = ({ item, addItem }) => {
+  const { imageUrl, name, price } = item;
+  return (
+    <div className="collection-item">
+      <div
+        style={{ backgroundImage: `url(${imageUrl}` }}
+        className="image"
+      ></div>
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <CustomButtom onClick={() => addItem(item)} inverted>
+        Add to cart
+      </CustomButtom>
     </div>
-    <CustomButtom inverted>Add to cart</CustomButtom>
-  </div>
-);
-
-export default CollectionItem;
+  );
+};
+const mapToDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
+export default connect(null, mapToDispatchToProps)(CollectionItem);
--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import './preview-collection.styles.scss'
-import CollectionItem from '../collection-item/collection-item.component'
+import React from "react";
+import "./preview-collection.styles.scss";
+import CollectionItem from "../collection-item/collection-item.component";
 
-const CollectionPreview = ({title, items}) => {
-    return(
-        <div className = 'collection-preview'>
-            <h1 className='title'>{title}</h1>
-            <div className="preview">
-                {items
-                .filter((item, idx) => idx < 4)
-                .map(({id, ...othersitemProps}) =>(
-                    <CollectionItem key={id} {...othersitemProps}/>
-                ))}
-            </div>
-        </div>
-    )
-}
+const CollectionPreview = ({ title, items }) => {
+  return (
+    <div className="collection-preview">
+      <h1 className="title">{title}</h1>
+      <div className="preview">
+        {items
+          .filter((item, idx) => idx < 4)
+          .map((item) => (
+            <CollectionItem key={item.id} item={item} />
+          ))}
+      </div>
+    </div>
+  );
+};
 
-export default CollectionPreview
+export default CollectionPreview;
