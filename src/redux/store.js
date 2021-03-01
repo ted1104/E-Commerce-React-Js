70987a712aef778e3c02b4aacbae64035b83155a
Teddy Walter <teddywalter2016@gmail.com>
2021-03-01 15:35:13 +0000

    persisit store configuration in store folder

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,11 @@ import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 
 import rootReducer from "./root-redux";
+import { persistStore } from "redux-persist";
 
 const middlewares = [logger];
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+const persistor = persistStore(store);
+
+export default { store, persistor };
