72e65e000e4b06ff89e6155e54991cf60961ed5c
Teddy Walter <teddywalter2016@gmail.com>
2021-03-09 12:09:32 +0000

    Get specific collection by url

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { connect } from "react-redux";
+import { selectCollection } from "../../redux/shop/shop.selectors";
 // import CollectionsItem from "../../components/cart-item/cart-item.component";
 
 import "./collection.styles.scss";
 
-const CollectionPage = ({ match }) => {
-  console.log(match.params.collectionId);
-
+const CollectionPage = ({ collection }) => {
+  console.log("======HEEEEEEEEEEERRRRRRRREEE=======");
+  console.log(collection);
   return (
     <div className="category">
       <h2>COLLECTIONS PAGE</h2>
@@ -13,4 +15,7 @@ const CollectionPage = ({ match }) => {
   );
 };
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+export default connect(mapStateToProps)(CollectionPage);
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -16,7 +16,7 @@ export const selectCollections = createSelector(
 );
 
 export const selectCollection = (collectionUrlParam) =>
-  createSelector([selectCollection], (collections) =>
+  createSelector([selectCollections], (collections) =>
     collections.find(
       (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
     )
