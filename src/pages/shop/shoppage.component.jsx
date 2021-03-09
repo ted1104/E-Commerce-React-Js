9e6f9971e6e4c36931001effbbcc3aa2399431a1
Teddy Walter <teddywalter2016@gmail.com>
2021-03-09 12:00:07 +0000

    selection for a category by name that is mapping to id

--- src/pages/shop/shoppage.component.jsx
+++ src/pages/shop/shoppage.component.jsx
@@ -4,7 +4,7 @@ import CollectionsOverview from "../../components/collections-overview/collectio
 import CollectionPage from "../collection/collection.component";
 
 const ShopPage = ({ match }) => {
-  console.log(match);
+  // console.log(match);
   return (
     <div className="shop-page">
       <Route exact path={`${match.path}`} component={CollectionsOverview} />
