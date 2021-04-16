d46645804d246d52f6eb4bc99fe2d8447eb01529
Teddy Walter <teddywalter2016@gmail.com>
2021-04-16 15:20:40 +0100

    convert shop page function to class

--- src/pages/shop/shoppage.component.jsx
+++ src/pages/shop/shoppage.component.jsx
@@ -3,14 +3,29 @@ import { Route } from "react-router-dom";
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 import CollectionPage from "../collection/collection.component";
 
-const ShopPage = ({ match }) => {
-  // console.log(match);
-  return (
-    <div className="shop-page">
-      <Route exact path={`${match.path}`} component={CollectionsOverview} />
-      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-    </div>
-  );
-};
+// const ShopPage = ({ match }) => {
+//   // console.log(match);
+//   return (
+//     <div className="shop-page">
+//       <Route exact path={`${match.path}`} component={CollectionsOverview} />
+//       <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+//     </div>
+//   );
+// };
+
+class ShopPage extends React.Component {
+  render() {
+    const { match } = this.props;
+    return (
+      <div className="shop-page">
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionPage}
+        />
+      </div>
+    );
+  }
+}
 
 export default ShopPage;
