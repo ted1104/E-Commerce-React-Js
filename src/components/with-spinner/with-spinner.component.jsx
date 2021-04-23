c444230d1d6337f2f94ddb453eace3d0580e96c1
Teddy Walter <teddywalter2016@gmail.com>
2021-04-23 17:30:50 +0100

    spinner done

--- src/components/with-spinner/with-spinner.component.jsx
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { SpinnerContainer, SpinnerOverlay } from "./with-spinner.styles";
 
-const WithSipinner = (WrappedComponent) => ({ isLoading, ...othersProps }) => {
-  return isLoading ? (
-    <SpinnerOverlay>
-      <SpinnerContainer></SpinnerContainer>
-    </SpinnerOverlay>
-  ) : (
-    WrappedComponent
-  );
+const WithSipinner = (WrappedComponent) => {
+  const Spinner = ({ isLoading, ...othersProps }) => {
+    return isLoading ? (
+      <SpinnerOverlay>
+        <SpinnerContainer></SpinnerContainer>
+      </SpinnerOverlay>
+    ) : (
+      <WrappedComponent {...othersProps} />
+    );
+  };
+
+  return Spinner;
 };
 
 export default WithSipinner;
